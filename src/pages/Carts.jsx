5fa8a26b3278f26773cb5173b3e76fe6aef6709f
Oyegbile Praise <oluwadhammieh@gmail.com>
2021-11-18 23:45:36 +0100

    cart updated

--- src/pages/Carts.jsx
+++ src/pages/Carts.jsx
@@ -2,6 +2,10 @@ import styled from 'styled-components'
 import Navbar from '../components/Navbar';
 import Announcement from '../components/Announcement';
 import Footer from '../components/Footer'
+
+import { Add, Remove } from '@material-ui/icons'
+
+
 const Container = styled.div``
 const Wrapper = styled.div`
     padding: 20px;
@@ -28,8 +32,68 @@ const TopText = styled.span`
     margin: 0px 10px;   
 `
 const Bottom = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const Summary = styled.div`
+    flex: 1;
+`
+const Info = styled.div`
+    flex: 1;
+`
+const Product = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const ProductDetail = styled.div`
+    flex: 2;
+    display: flex;
+`
+const ProductAmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+`
+const ProductAmount = styled.div`
+    font-size: 24px;
+    margin: 5px;
+`
+const ProductPrice = styled.div`
+    font-size: 30px;
+    font-weight: 200
+`
+const Hr = styled.div`
+    background-color: #eee;
+    border: none;
+    height: 1px;
+`
+const Image = styled.img`
+    width: 200px;
+`
+const Details = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`
+const ProductName = styled.span`
 
 `
+const ProductId = styled.span``
+const ProductColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+`
+const ProductSize = styled.span``
+const PriceDetail = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column
+    align-items: center;
+    justify-content: center;
+`
+const Price = styled.div``
 
 const Carts = () => {
     return (
@@ -46,11 +110,51 @@ const Carts = () => {
                         <TopText>Shopping Bag (2)</TopText>
                         <TopText>Your Wishlist (0)</TopText>
                     </TopTexts>
-                    <TopButton>
+                    <TopButton style={{ backgroundColor: 'black', color: 'white' }}>
                         CHECKOUT NOW
                     </TopButton>
                 </Top>
-                <Bottom></Bottom>
+                <Bottom>
+                    <Info>
+                        <Product>
+                            <ProductDetail>
+                                <Image src='http://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A' />
+                                <Details>
+                                    <ProductName><b>Products: </b>JESSIE THUNDER SHOES</ProductName>
+                                    <ProductId><b>ID: </b>72325724879</ProductId>
+                                    <ProductColor style={{ backgroundColor: 'black' }} />
+                                    <ProductSize><b>Size: </b>37.5</ProductSize>
+                                </Details>
+                            </ProductDetail>
+                            <PriceDetail></PriceDetail>
+                            <ProductAmountContainer>
+                                <Add />
+                                <ProductAmount>2</ProductAmount>
+                                <Remove />
+                            </ProductAmountContainer>
+                            <ProductPrice>$ 30</ProductPrice>
+                        </Product>
+                        {/* <Hr /> */}
+                        <Product>
+                            <ProductDetail>
+                                <Image src='http://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A' />
+                                <Details>
+                                    <ProductName><b>Products: </b>JESSIE THUNDER SHOES</ProductName>
+                                    <ProductId><b>ID: </b>72325724879</ProductId>
+                                    <ProductColor style={{ backgroundColor: 'black' }} />
+                                    <ProductSize><b>Size: </b>37.5</ProductSize>
+                                </Details>
+                            </ProductDetail>
+                            <ProductAmountContainer>
+                                <Add />
+                                <ProductAmount>2</ProductAmount>
+                                <Remove />
+                            </ProductAmountContainer>
+                            <ProductPrice>$ 30</ProductPrice>
+                        </Product>
+                    </Info>
+                    <Summary>Summary</Summary>
+                </Bottom>
             </Wrapper>
             <Footer />
         </Container>
