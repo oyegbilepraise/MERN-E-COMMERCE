918311f8ed4fef7076798a2d1ea16983afd540df
Oyegbile Praise <oluwadhammieh@gmail.com>
2021-11-18 16:31:17 +0100

    cart added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 // import Home from "./pages/Home";
 // import Product from './pages/Product'
 // import ProductList from './pages/ProductList';
-
-import Login from "./pages/Login";
-
+// import Login from "./pages/Login";
 // import Register from "./pages/Register";
 
+import Cart from "./pages/Carts";
+
 const App = () => {
-  return <Login />
+  return <Cart />
 };
 
 export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,20 +6,20 @@ const Container = styled.div`
     background:linear-gradient(
         rgba(255, 255, 255, 0.5),
         rgba(255, 255, 255, 0.5)),
-        url('https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940') center;
+        url('https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940') center;
         background-size: cover;
         display: flex;
         align-items: center;
         justify-content: center;
 `
 const Wrapper = styled.div`
-        width: 40%;
+        width: 25%;
         padding: 20px;
         background-color: white;
 `
 const Form = styled.form`
         display: flex;
-        flex-wrap: wrap;
+        flex-direction: column;
 `
 const Title = styled.h1`
         font-size: 24px;
@@ -28,7 +28,7 @@ const Title = styled.h1`
 const Input = styled.input`
         flex: 1;
         min-width: 40%;
-        margin: 20px 10px 0px 0px;
+        margin: 10px 0px;
         padding: 10px;
 `
 
@@ -39,8 +39,14 @@ const Button = styled.button`
     background-color: teal;
     color: white;
     cursor: pointer;
+    margin-bottom: 10px;
 `
-const Link = styled.a``
+const Link = styled.a`
+        margin: 5px 0px;
+        font-size: 12px;
+        text-decoration: underline;
+        cursor: pointer;
+`
 const Login = () => {
     return (
         <Container>
