f2781eb7ec549bc95cf4c41ed39e0fd9a5431bef
Oyegbile Praise <oluwadhammieh@gmail.com>
2021-12-19 23:56:29 +0100

    bug @ checkout route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Product from './pages/Product'
-import ProductList from './pages/ProductList';
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
@@ -15,14 +15,14 @@ const App = () => {
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path='products/:category' element={<ProductList />} />
-        <Route path='product/:id' element={<Product />} />
-        <Route path='cart' element={<Cart />} />
-        <Route path='login' element={<Login />} />
+        <Route path="products/:category" element={<ProductList />} />
+        <Route path="product/:id" element={<Product />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
       </Routes>
     </Router>
-  )
+  );
 };
 
 export default App;
--- src/pages/Carts.jsx
+++ src/pages/Carts.jsx
@@ -6,10 +6,13 @@ import { mobile } from "../responsive";
 import { Add, Remove } from "@material-ui/icons";
 import { useSelector } from "react-redux";
 import StripeCheckout from "react-stripe-checkout";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { userRequest } from "../requestMethods";
+import { useNavigate } from "react-router-dom";
 
-const KEY = process.env.REACT_APP_STRIPE;
-console.log(KEY);
+// const KEY = process.env.REACT_APP_STRIPE;
+const KEY =
+  "pk_test_51Jy1RuIJoqzY8FrrKz93hdqzlk4fdYgY5t6s1hZjRJQHiMngdTCDQhHaXECGdMZLV5kCNnvmdV9Ky57Rz0RhkSbu00QcPyhMbn";
 
 const Container = styled.div``;
 const Wrapper = styled.div`
@@ -132,10 +135,26 @@ const Button = styled.button`
 const Carts = () => {
   const cart = useSelector((state) => state.cart);
   const [stripeToken, setStripeToken] = useState(null);
+  const navigate = useNavigate();
+
   const onToken = (token) => {
     setStripeToken(token);
   };
-  console.log(stripeToken);
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await userRequest.post("/checkout/payment", {
+          tokenId: stripeToken.id,
+          amount: 500,
+        });
+        navigate.push("/success", { data: res.data });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    stripeToken && makeRequest();
+  }, [stripeToken, cart.total, navigate]);
   return (
     <Container>
       <Navbar />
